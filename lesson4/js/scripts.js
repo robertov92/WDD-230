2600e84fb9fc2a0198277e22941c21f4597cf563
robertov92 <vil13004@byui.edu>
2021-01-29 19:20:46 -0600

    map api

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,11 +1,26 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+
+var today = new Date();
+var weekDay = days[today.getDay()];
+var day = String(today.getDate()).padStart(2, '0');
+var month = months[today.getMonth()];
+var year = today.getFullYear();
+
+todayFormatted = weekDay + ', ' + day + ' ' + month + ' ' + year;
+document.write(todayFormatted);
+
 
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function initMap() {
+    var location = { lat: 42.096291, lng: -111.876640 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+    var marker = new google.maps.Marker({ position: location, map: map });
 }
