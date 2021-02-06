8c1c322a664f45e171764c9e9446a1a66a1b95de
robertov92 <vil13004@byui.edu>
2021-02-06 14:08:59 -0600

    Final commit to submit lesson 5

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,4 @@
+//Formatted date
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -11,7 +12,12 @@ var year = today.getFullYear();
 todayFormatted = weekDay + ', ' + day + ' ' + month + ' ' + year;
 document.write(todayFormatted);
 
-
+//Responsive navigation menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+//Pancake reminder
+if (today.getDay() == 5) {
+    document.getElementById("prestonPancakes").style.display = "block";
 }
